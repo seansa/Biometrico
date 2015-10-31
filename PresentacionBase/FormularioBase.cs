779d164f1ad84d38a74d8ac1957f0fb41ab19284
Widcket <zeta@widcket.com>
2015-10-31 05:48:38 -0300

    [Rescue] Funciona

--- PresentacionBase/FormularioBase.cs
+++ PresentacionBase/FormularioBase.cs
@@ -38,7 +38,15 @@ namespace PresentacionBase
             this.BackColor = Colores.ColorFondoFormulario;
             this.fuentes = new PrivateFontCollection();
 
-            
+            byte[] fontData = RecursosCompartidos.HelveticaNeue_Thin;
+            IntPtr fontPtr = System.Runtime.InteropServices.Marshal.AllocCoTaskMem(fontData.Length);
+            System.Runtime.InteropServices.Marshal.Copy(fontData, 0, fontPtr, fontData.Length);
+            uint dummy = 0;
+            fonts.AddMemoryFont(fontPtr, RecursosCompartidos.HelveticaNeue_Thin.Length);
+            AddFontMemResourceEx(fontPtr, (uint)RecursosCompartidos.HelveticaNeue_Thin.Length, IntPtr.Zero, ref dummy);
+            System.Runtime.InteropServices.Marshal.FreeCoTaskMem(fontPtr);
+
+            myFont = new Font(fonts.Families[0], 20.0F);
         }
 
         public FormularioBase(Color colorFondoForm)             
