dc20381ad648fc0ca1e16923609b908b9cb3ef62
Diego God <perucag@hotmail.com>
2015-11-02 16:32:36 -0300

    basura2-11

--- Presentacion.Seguridad/LogIn.cs
+++ Presentacion.Seguridad/LogIn.cs
@@ -176,6 +176,7 @@ namespace Presentacion.Seguridad
         {
             this.txtUsuario.Text = "ADMIN";
             this.txtPassword.Text = "123";
+            lblTitulo.Font = new FormularioBase().myFont;
         }
     }
 }
