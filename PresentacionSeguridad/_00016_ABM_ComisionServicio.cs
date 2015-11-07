1fc121e38ec92c07d7f3266cb6fe842ec9ad59b8
Rita Zerrizuela <zeta@widcket.com>
2015-11-06 21:27:43 -0300

    [Comisiones de Servicio] Agregado formulario ABM

---
+++ PresentacionSeguridad/_00016_ABM_ComisionServicio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentacionRecursoHumano
+{
+    public partial class _00016_ABM_ComisionServicio : PresentacionBase.FormularioBase
+    {
+        public _00016_ABM_ComisionServicio()
+        {
+            InitializeComponent();
+        }
+        public _00016_ABM_ComisionServicio(string titulo) : this()
+        {
+            this.Text = titulo;
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
