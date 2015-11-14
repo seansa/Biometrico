afdf6dad9c35f12f5ef8bb1aa040e0d24b63e05a
Santiago <sna1988@gmail.com>
2015-11-14 02:25:32 -0300

    [Metodo Insertar de Novedades]

--- ServicioSeguridad/NovedadAgente/INovedadAgenteServicio.cs
+++ ServicioSeguridad/NovedadAgente/INovedadAgenteServicio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Servicio.RecursoHumano.NovedadAgente.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Servicio.RecursoHumano.NovedadAgente
 {
     public interface INovedadAgenteServicio
     {
+        void Insertar(NovedadAgenteDTO novedadAgente);
     }
 }
--- ServicioSeguridad/NovedadAgente/NovedadAgenteServicio.cs
+++ ServicioSeguridad/NovedadAgente/NovedadAgenteServicio.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AccesoDatos;
+using Servicio.RecursoHumano.NovedadAgente.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,33 @@ namespace Servicio.RecursoHumano.NovedadAgente
 {
     public class NovedadAgenteServicio : INovedadAgenteServicio
     {
+        public void Insertar(NovedadAgenteDTO novedadAgente)
+        {
+            try
+            {
+                using (var _context=new ModeloBometricoContainer())
+                {
+                    var _novedad = new Novedad()
+                    {
+                        AgenteId=novedadAgente.AngenteId,
+                        TipoNovedadId=novedadAgente.TipoNovedadId,
+                        FechaDesde=novedadAgente.FechaDesde,
+                        FechaHasta=novedadAgente.FechaHasta,
+                        HoraDesde=novedadAgente.HoraDesde,
+                        HoraHasta=novedadAgente.HoraHasta,
+                        Observacion=novedadAgente.Observacion
+                        
 
+                    };
+                    _context.Novedades.Add(_novedad);
+                    _context.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
