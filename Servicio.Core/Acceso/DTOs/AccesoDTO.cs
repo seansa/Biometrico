ee6fcaf036793899aa95a01058c47d77d9cc4bb5
Rita Zerrizuela <zeta@widcket.com>
2015-11-15 17:06:05 -0300

    [Acceso DTO] Agregado el DTO de acceso de la rama de santiago

--- Servicio.Core/Acceso/DTOs/AccesoDTO.cs
+++ Servicio.Core/Acceso/DTOs/AccesoDTO.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace Servicio.Core.Acceso.DTOs
 {
-   public class AccesoDTO
+    public class AccesoDTO
     {
 
         public long Id { get; set; }
 
         public long AgenteId { get; set; }
         public DateTime FechaHora { get; set; }
-        public string  TipoAcceso { get; set; }
+        public string TipoAcceso { get; set; }
         public string Fecha { get { return FechaHora.ToShortDateString(); } }
         public string Hora { get { return FechaHora.ToShortTimeString(); } }
         public int NumeroReloj { get; set; }
